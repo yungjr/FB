efc15fcb5191d251e364a2b45b20a3a341d827d3
1freed <105211017+1freed@users.noreply.github.com>
2022-08-19 13:14:42 +0800

    Update XBiubiu.java

--- app/src/main/java/com/github/catvod/spider/XBiubiu.java
+++ app/src/main/java/com/github/catvod/spider/XBiubiu.java
@@ -223,40 +223,11 @@ public class XBiubiu extends Spider {
                         playList.add(TextUtils.join("#", vodItems));
                     } catch (Throwable th) {
                         th.printStackTrace();
-                    break;
+                    }
                 }
+            } else {
+                playList.add(idInfo[0] + "$" + idInfo[2]);
             }
-            
-           ArrayList<String> playFrom = new ArrayList<>();
-           String xlparseContent = html;
-           if(getRuleVal("xlbiaotiqian").isEmpty() && getRuleVal("xlbiaotihou").isEmpty()){
-           
-            for (int i = 0; i < playList.size(); i++) {
-                playFrom.add("播放列表" + (i + 1));
-            }
-           }else{
-           
-           boolean xlshifouercijiequ = getRuleVal("xlshifouercijiequ").equals("1");
-            if (xlshifouercijiequ) {
-                String xljiequqian = getRuleVal("xljiequqian");
-                String xljiequhou = getRuleVal("xljiequhou");
-                xlparseContent = subContent(html, xljiequqian, xljiequhou).get(0);
-            }
-            
-            String xljiequshuzuqian = getRuleVal("xljiequshuzuqian");
-            String xljiequshuzuhou = getRuleVal("xljiequshuzuhou");
-            ArrayList<String> xljiequContents = subContent(xlparseContent, xljiequshuzuqian, xljiequshuzuhou);
-            for (int i = 0; i < playList.size(); i++) {
-                try {
-                     String xltitle = subContent(xljiequContents.get(i), getRuleVal("xlbiaotiqian"), getRuleVal("xlbiaotihou")).get(0);                     
-                     playFrom.add(xltitle);
-                } catch (Throwable th) {
-                    th.printStackTrace();
-                    break;
-                }
-            }           
-           
-           }
             String cover = idInfo[1], title = idInfo[0], area = "";
             String director = "";
             String actor = "";
@@ -320,7 +291,11 @@ public class XBiubiu extends Spider {
             vod.put("vod_director", director);
             vod.put("vod_content", desc);
 
-            
+            ArrayList<String> playFrom = new ArrayList<>();
+
+            for (int i = 0; i < playList.size(); i++) {
+                playFrom.add("播放列表" + (i + 1));
+            }
 
             String vod_play_from = TextUtils.join("$$$", playFrom);
             String vod_play_url = TextUtils.join("$$$", playList);
